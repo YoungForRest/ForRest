a89c78fdf443486785edd1c295ca83239c87e122
YoungForRest <39110120+YoungForRest@users.noreply.github.com>
2019-01-02 16:51:42 +0800

    Initial commit

---
+++ src/main/java/com/zys/listener/ApplicationReadyListener.java
@@ -0,0 +1,20 @@
+package com.zys.listener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by YoungTH on 2018/10/26.
+ */
+@Component
+public class ApplicationReadyListener implements ApplicationListener<ApplicationReadyEvent> {
+    private Logger logger = LoggerFactory.getLogger(ApplicationReadyListener.class);
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
+        logger.info(">>>>>>>系统启动完成<<<<<<<");
+    }
+}
