2a10d27c7c74c716e911859555cd170abca07b0f
YoungForRest <39110120+YoungForRest@users.noreply.github.com>
2019-01-03 15:44:01 +0800

    20190103
    20190103

---
+++ target/classes/com/zys/BootdemoApplication.java
@@ -0,0 +1,12 @@
+package com.zys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootdemoApplication.class, args);
+	}
+}
