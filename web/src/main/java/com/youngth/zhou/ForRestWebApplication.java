c7edc90fefe27c70365d8c3e7cb59aa46a82104e
YoungForRest <39110120+YoungForRest@users.noreply.github.com>
2019-04-10 21:24:54 +0800

    单模块重构  引入maven父子模块结构

--- web/src/main/java/com/youngth/zhou/ForRestWebApplication.java
+++ web/src/main/java/com/youngth/zhou/ForRestWebApplication.java
@@ -3,10 +3,15 @@ package com.youngth.zhou;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author YoungTH
+ * @date 2019/4/2
+ * springboot 启动入口
+ */
 @SpringBootApplication
-public class ForRestApplication {
+public class ForRestWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ForRestApplication.class, args);
+		SpringApplication.run(ForRestWebApplication.class, args);
 	}
 }
