ee4b435f50e216c28a43f1e465249b4bc12df6a3
YoungForRest <39110120+YoungForRest@users.noreply.github.com>
2019-04-11 09:29:34 +0800

    web模块引入entity模块实体

---
+++ entity/src/main/java/com/youngth/zhou/entity/TestEntity.java
@@ -0,0 +1,13 @@
+package com.youngth.zhou.entity;
+
+import lombok.Data;
+
+/**
+ * @author YoungTH
+ * @date 2019/4/11
+ */
+@Data
+public class TestEntity {
+    private String name;
+    private Integer age;
+}
